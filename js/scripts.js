f5723647662ce4e9b859a10456273d988571e2fd
Katrina Hirsch <katrina.hirsch@gmail.com>
2017-04-10 17:12:03 -0700

    add function to handle range update

--- js/scripts.js
+++ js/scripts.js
@@ -22,9 +22,15 @@ function skip() {
   video.currentTime += parseFloat(this.dataset.skip);
 }
 
+function handleRangeUpdate() {
+  video[this.name] = this.value;
+}
+
 //Event Listeners
 video.addEventListener('click', togglePlay);
 video.addEventListener('play', updateButton);
 video.addEventListener('pause', updateButton);
 toggle.addEventListener('click', togglePlay);
 skipButtons.forEach(button => button.addEventListener('click', skip));
+ranges.forEach(range => range.addEventListener('change', handleRangeUpdate));
+ranges.forEach(range => range.addEventListener('mousemove', handleRangeUpdate))
